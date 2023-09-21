fed1ec1c2e87ccd64fafaef2b64d21e3c458c912
Francesco <stelk.mc@gmail.com>
2023-09-21 15:52:59 +0200

    WIP

--- src/main/java/tech/ailef/dbadmin/test/SpringBootDbAdminTestProjectApplication.java
+++ src/main/java/tech/ailef/dbadmin/test/SpringBootDbAdminTestProjectApplication.java
@@ -1,15 +1,13 @@
 package tech.ailef.dbadmin.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import tech.ailef.dbadmin.DbAdminAutoConfiguration;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"tech.ailef.dbadmin"})
-@EnableJpaRepositories(basePackages = {"tech.ailef.dbadmin"})
-@EntityScan(basePackages = {"tech.ailef.dbadmin"})
+@ImportAutoConfiguration(DbAdminAutoConfiguration.class)
 public class SpringBootDbAdminTestProjectApplication {
 
 	public static void main(String[] args) {
