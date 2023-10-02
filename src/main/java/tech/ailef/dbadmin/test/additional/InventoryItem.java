e26d05f64cde585366b177da9f18b60257e71e66
Francesco <stelk.mc@gmail.com>
2023-10-02 09:14:53 +0200

    Added additional package to scan

---
+++ src/main/java/tech/ailef/dbadmin/test/additional/InventoryItem.java
@@ -0,0 +1,41 @@
+package tech.ailef.dbadmin.test.additional;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="inventory")
+public class InventoryItem {
+	@Id
+	private String id;
+	
+	private String name;
+	
+	private Boolean available;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+	
+}
