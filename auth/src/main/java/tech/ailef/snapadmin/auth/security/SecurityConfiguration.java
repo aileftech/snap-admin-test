1c2684e38d094b6b28581370428aaa27a7c3d38a
Francesco <stelk.mc@gmail.com>
2023-11-09 14:54:20 +0100

    Security Configuration comments

--- auth/src/main/java/tech/ailef/snapadmin/auth/security/SecurityConfiguration.java
+++ auth/src/main/java/tech/ailef/snapadmin/auth/security/SecurityConfiguration.java
@@ -11,22 +11,22 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandlerImpl;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import tech.ailef.snapadmin.external.SnapAdminProperties;
 
+/**
+ * This class shows how to secure your SnapAdmin using a standard
+ * Spring Security configuration. I particular, look at the {@link SecurityFilterChain}
+ * bean defined in the {@link #filterChain(HttpSecurity)} method to see
+ * how to require authentication and authorization to access specific SnapAdmin
+ * routes and features.
+ */
 @Configuration
 public class SecurityConfiguration {
 	@Autowired
 	private SnapAdminProperties properties;
 	
-	@Bean
-	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-		return new MvcRequestMatcher.Builder(introspector);
-	}
-
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -46,14 +46,15 @@ public class SecurityConfiguration {
 	}
 
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		String baseUrl = properties.getBaseUrl();
 		
 	    return http.authorizeHttpRequests(auth -> {
-	        // POST methods (create, edit and delete) require ADMIN role
-	    	// Note that with this configuration users will still be able to access the edit/create page
-	    	// but they will get a Forbidden error after submitting the form if they are not authorized.
-	    	// You can also stop the serving of these pages altogether by customizing the route matchers
+	        /* POST methods (create, edit and delete) require ADMIN role
+	    	 * Note that with this configuration users will still be able to access the edit/create page
+	    	 * but they will get a Forbidden error after submitting the form if they are not authorized.
+	    	 * You can also stop the serving of these pages altogether by customizing the route matchers
+	    	 */
 	        auth.requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/" + baseUrl + "/**")).hasAuthority("ADMIN")
 	            // Read-only SnapAdmin routes require authentication (any role)
 	            .requestMatchers(AntPathRequestMatcher.antMatcher("/" + baseUrl + "/**")).authenticated()
@@ -61,16 +62,14 @@ public class SecurityConfiguration {
 	            .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll();
 	    })
 	    .formLogin(l -> l.loginPage("/login").permitAll())
-	    /*
-	     * This custom exception handling code is only needed if you want to have
+	    /* This custom exception handling code is only needed if you want to have
 	     * nicer Forbidden error pages, for cases when a user tries to perform an
-	     * action they don't have the correct privileges for.
-	     * For example, in the previous configuration a user without "ADMIN" role
-	     * trying to edit/create items.
+	     * action they don't have the correct privileges for (e.g., in the previous
+	     * configuration a user without ADMIN role trying to edit/create items).
 	     * The exception handling is delegated to the default handler if the
 	     * error didn't occur on a SnapAdmin route. You can further customize this
-	     * according to your need or ignore it entirely. In this last scenario, your 
-	     * default Forbidden page will be shown even for errors occurring inside
+	     * according to your needs or just not use it. In this last scenario, your 
+	     * default access denied handler will be used even for errors occurring inside
 	     * SnapAdmin.
 	     */
 	    .exceptionHandling(e -> e.accessDeniedHandler((req, res, ex) -> {
